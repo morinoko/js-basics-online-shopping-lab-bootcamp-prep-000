3ef015a83c9667e1f1019760a6ab57382bdc42ef
Felice Forby <feliceforby@gmail.com>
2018-01-02 07:01:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,5 +72,10 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === '') {
+    console.log( "Sorry, we don't have a credit card on file for you." );
+  } else {
+    let total = total();
+    console.log( `Your total cost is $${total}, which will be charged to the ` );
+  }
 }
